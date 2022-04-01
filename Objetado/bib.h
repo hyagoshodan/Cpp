80f998f56d24faa2255672b044bf435e4da84959
Hyago Shodan <hyagodejesus@outlook.com.br>
2022-04-01 09:40:01 -0300

    Orientado a objetos

---
+++ Objetado/bib.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+class Ola
+{
+    public:
+        void MostrarMessagem()
+        {
+            cout << "Olá\n";
+        }
+};
--- Objetado/main.cpp
+++ Objetado/main.cpp
@@ -1,1 +1,16 @@
 #include <iostream>
+
+using namespace std;
+
+#include "bib.h"
+
+int main(){
+    Ola teste;
+  
+    cout << "Iniciando a classe...\n";
+
+    teste.MostrarMessagem();
+
+
+    return 0;
+}
