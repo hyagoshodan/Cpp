5ca20cc2f0817d9dd8c6ed110f564ad0c7406101
Hyago Shodan <hyagodejesus@outlook.com>
2022-04-06 09:04:12 -0300

    Exercicio de instrução condicional

---
+++ Estruturado/if_sete.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero = 3;
+
+    if ( numero != 7 )
+    {
+        cout << "O número não é igual a 7\n";
+    }   
+        else
+        {
+            cout << "Terminado!\n";
+        }
+}
